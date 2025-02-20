d7b6ceeae7d196ac41e2ad3fe8f96f0d3727f764
hamilton <hamilton-souza@outlook.com.br>
2025-02-20 10:31:20 -0300

    feat(validation): adicionado validação para verificar se o usuário já está cadastrado

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm'
 import { db } from '../drizzle/clientDrizzle'
 import { tableSubscriptions } from '../drizzle/schema/tableSubscriptions'
 
@@ -10,6 +11,18 @@ export async function subscribeToEvent({
   name,
   email,
 }: SubscribeToEventParams) {
+  // validação do email do usuário para verificar se ele já está cadastrado
+  const subscribers = await db
+    .select()
+    .from(tableSubscriptions)
+    .where(eq(tableSubscriptions.email, email))
+
+  if (subscribers.length > 0) {
+    return {
+      subscriberId: subscribers[0].id,
+    }
+  }
+
   // adicionando um usuário no banco
   const result = await db
     .insert(tableSubscriptions)
