1faa56bb8ac77b5086af89001c540c6204ff9893
hamilton <hamilton-souza@outlook.com.br>
2025-02-20 11:06:58 -0300

    feat(invites): adicionado a funcionalidade de verificar se o inscrito atual está se inscrevendo no evento através de um link de indicação

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,15 +1,18 @@
 import { eq } from 'drizzle-orm'
 import { db } from '../drizzle/clientDrizzle'
 import { tableSubscriptions } from '../drizzle/schema/tableSubscriptions'
+import { redis } from '../redis/clientRedis'
 
 interface SubscribeToEventParams {
   name: string
   email: string
+  referrerId?: string | null
 }
 
 export async function subscribeToEvent({
   name,
   email,
+  referrerId,
 }: SubscribeToEventParams) {
   // validação do email do usuário para verificar se ele já está cadastrado
   const subscribers = await db
@@ -31,6 +34,11 @@ export async function subscribeToEvent({
       email,
     })
     .returning()
+  // verificando se o usuário vem de uma indicação
+  if (referrerId) {
+    await redis.zincrby('referral:ranking', 1, referrerId)
+  }
+
   // retornando o id do usuário
   const subscriber = result[0]
   return {
